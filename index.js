bcbf0da1669234efbcdd9a7906cef2dea0439358
anhakobyan25 <150049080+anhakobyan25@users.noreply.github.com>
2024-04-15 19:07:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 let pause = document.getElementById('pause');
 let start = document.getElementById('start');
-
 let active = 0;
 let itemLength = items.length - 1;
 let isPaused = false;
 let refreshSlider;
-
-
 function autoSlide() {
     if (!isPaused) {
         if (active + 1 > itemLength) {
@@ -22,18 +19,17 @@ function autoSlide() {
         reloadSlider();
     }
 }
-
 function reloadSlider() {
     let checkleft = items[active].offsetLeft;
     list.style.left = (-checkleft) + 'px';
-    
     let lastActive = document.querySelector('.slider .dots li.active');
     lastActive.classList.remove('active');
     dots[active].classList.add('active');
 }
-
-refreshSlider = setInterval(autoSlide, 4000);
-
+function startInterval() {
+   refreshSlider = setInterval(autoSlide, 4000);
+}
+startInterval()
 next.addEventListener('click', function() {
     if (active + 1 > itemLength) {
         active = 0;
@@ -42,7 +38,6 @@ next.addEventListener('click', function() {
     }
     reloadSlider();
 });
-
 prev.addEventListener('click', function() {
     if (active - 1 < 0) {
         active = itemLength;
@@ -51,11 +46,11 @@ prev.addEventListener('click', function() {
     }
     reloadSlider();
 });
-
 start.addEventListener('click', function() {
     isPaused = false;
+    refreshSlider = setInterval(autoSlide, 1000);
 });
-
 pause.addEventListener('click', function() {
     isPaused = true;
+    clearInterval(refreshSlider)
 });
